ae6c7a26c36ea64d816ced8dcef91936df9115b4
eraykosebalaban <kosebalabaneray@gmail.com>
2022-01-17 01:27:10 +0300

    Pic16f877 ile PWM DC Motor Hız Kontrolü

---
+++ Pic16f877 ile PWM DC Motor Hız Kontrolü/PWM ile DC Motor Hiz Kontrolu.c
@@ -0,0 +1,70 @@
+#include<16f877.h>
+
+#device ADC = 8;
+
+#use delay(clock = 4000000)
+
+#use fast_io(a)
+#use fast_io(b)
+#use fast_io(c)
+
+#define use_portb_lcd TRUE
+#include<lcd.c>
+
+unsigned int deger;
+float voltaj;
+
+void main(){
+setup_psp(PSP_DISABLED);
+setup_timer_1(T1_DISABLED);
+setup_ccp2(CCP_OFF);
+
+setup_timer_2(T2_DIV_BY_4,255,1);
+setup_CCP1(CCP_PWM); // PWM kullanacaðýmýz için PWM dedik.
+
+setup_adc(adc_clock_div_32);
+setup_adc_ports(ALL_ANALOG);
+
+set_tris_a(0x20);
+set_tris_b(0x00);
+set_tris_c(0x00);
+
+set_adc_channel(4); //AN4 kullanýyoruz.
+
+output_c(0x00);
+
+lcd_init();
+
+set_pwm1_duty(0); //ccp1 kullandýðýmýz için pwm1. pwm hemen iletime geçmesin diye 0 dedik.
+
+while(true){
+
+deger = read_adc();
+voltaj = deger / 51.0; // 255/51 den 5V gelmesi için
+
+output_high(pin_c3); // motor ileri
+output_low(pin_c4); // motor geri
+
+
+
+set_pwm1_duty(deger); // eðer deðer 255 ten büyükse 255 e eþitle ve onu pwm1 duty e yazdýr.
+
+printf(lcd_putc,"\fVoltaj=%fV",voltaj);
+lcd_gotoxy(1,2);
+printf(lcd_putc,"Deger=%u",deger);
+delay_ms(500);
+
+
+}
+
+}
+
+
+
+
+
+
+
+
+
+
